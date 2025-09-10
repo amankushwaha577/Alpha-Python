884d73d47efc72d1a4e0e995cde6d5c1a9483a59
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-09-11 01:38:29 +0530

    init

---
+++ 105. Function/c. tricks.py
@@ -0,0 +1,8 @@
+# Que. Default vs keyword arguments?
+# → Default = predefined (def f(x=5)), keyword = pass by name (f(x=10)).
+#
+# Que. What are *args and **kwargs?
+# → *args = variable-length positional args, **kwargs = variable-length keyword args.
+#
+# Que. What are lambda functions?
+# Anonymous one-line functions → lambda x: x*x.
