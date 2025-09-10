cd255d2d718b8ec6f58c3179810b4d570a4dfd31
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-09-11 01:44:31 +0530

    init

---
+++ 110. OOPS/a. basic.py
@@ -0,0 +1,8 @@
+# Que. What are classes and objects?
+# → Class = blueprint, Object = instance.
+#
+# Que. What is inheritance in Python?
+# → Mechanism to use parent class properties.
+#
+# Que. Difference between @classmethod, @staticmethod, @property?
+# → classmethod → cls access, staticmethod → no self/cls, property → getter/setter.
